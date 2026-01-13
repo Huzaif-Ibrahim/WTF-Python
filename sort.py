57586ecd11a8c4e5082af9d45caedfccd42cfa99
Huzaif Ibrahim <i.huzaif.ibrahim@gmail.com>
2026-01-13 19:48:21 +0530

    refactor: uncommented the commented areas

--- sort.py
+++ sort.py
@@ -1,14 +1,11 @@
-# arr = [19,40,1,3,7,0,4,6,8,10]
-# print(f'Original : {arr}')
+arr = [19,40,1,3,7,0,4,6,8,10]
+print(f'Original : {arr}')
 
-# n = len(arr)
+n = len(arr)
 
-# for i in range(n):
-#     for j in range(n):
-#         if arr[i] < arr[j]:
-#             arr[i], arr[j] = arr[j], arr[i]
+for i in range(n):
+    for j in range(n):
+        if arr[i] < arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
 
-# print(f'Sorted : {arr}')
-
-a = [1,2,3,4,5,6]
-print(len(a))
+print(f'Sorted : {arr}')
