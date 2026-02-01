edbedc7998344b9f29d6b696e925561b871c720f
Huzaif Ibrahim <i.huzaif.ibrahim@gmail.com>
2026-02-01 14:25:09 +0530

    Binary Search

---
+++ binarySearch.py
@@ -0,0 +1,28 @@
+import time
+
+def binarySearch(a , low, high, key):
+    if low > high or key not in a:
+        print("Unseccessful operation")
+        return
+    mid = (high+low)//2
+    if(a[mid] == key):
+        print(f'The key is at index {mid}')
+        return 
+    elif(key > a[mid]):
+        binarySearch(a, mid+1, high,key)
+        # return print(f'The key is at index {a[mid]}')
+    else:
+        binarySearch(a, low, mid-1,key)
+        
+a = []
+n = int(input("How many elements do you want in a array : "))
+
+for i in range(n):
+    a.append(int(input(f"Enter the {i+1} element : ")))
+    
+key = int(input("Enter the key: "))
+start = time.time()
+binarySearch(a, 0, len(a) - 1, key)
+end = time.time()
+
+print(f'Time took for the operation : {end-start}')
