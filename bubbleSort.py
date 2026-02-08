95eb40e0675bf420b1572fc8c893e6e9933b8a9b
Huzaif Ibrahim <i.huzaif.ibrahim@gmail.com>
2026-02-08 10:51:51 +0530

    Bubble sort practice

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,33 +1,35 @@
 # import matplotlib.pyplot as plt
 
-# def bubbleSort(array):
-#     n = len(array)
+# def bubbleSort(arr):
+#     n = len(arr)
 #     for i in range(n-1):
+#         swapped = False
 #         for j in range(n-1-i):
-#             if array[j] > array[j+1]:
-#                 array[j], array[j+1] = array[j+1], array[j]
-#         print(array)
-#     return array
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#                 swapped = True
+#         if swapped == False:
+#             break
+#     return arr
 
-# array = []
-# n = int(input('How many elements do you want in the array : '))
+# a = []
+# n = int(input("Enter the elements you want in array: "))
 # for i in range(n):
-#     array.append(int(input(f"Enter {i+1} element : ")))
-# print(f'Array before swapping : {array}')
-# array = bubbleSort(array)
-# print(f"Array after sorting: {array}")
+#     a.append(int(input(f"Enter {i} element: ")))
+    
+# print(f"Array before swapping: {a}")
+# bubbleSort(a)
+# print(f'Array after swapping {a}')
 
-# x=list(range(0, 10000))
-# y = list([y*y for y in x])
-
-# plt.plot(x, y)
-# plt.title("Bubble sort - Time Complexity is O(n\u00b2)")
+# x = list(range(1,10000))
+# plt.plot(x, [y*y for y in x])
+# plt.title("Time complexity of bubble sort is O(n\u00b2)")
 # plt.xlabel("Input")
 # plt.ylabel("Time")
 # plt.show()
 
-
 import matplotlib.pyplot as plt
+import time
 
 def bubbleSort(arr):
     n = len(arr)
@@ -42,17 +44,20 @@ def bubbleSort(arr):
     return arr
 
 a = []
-n = int(input("Enter the elements you want in array: "))
+n = int(input('Enter the elements you want in array: '))
 for i in range(n):
-    a.append(int(input(f"Enter {i} element: ")))
+    a.append(int(input(f'Enter {i+1} element: ')))
     
-print(f"Array before swapping: {a}")
-bubbleSort(a)
-print(f'Array after swapping {a}')
+print(f'Your array is: {a}')
+start = time.time()
+a = bubbleSort(a)
+end = time.time()
+print(f'Sorted array: {a}')
+print(f'Runtime is {end-start}')
 
 x = list(range(1,10000))
 plt.plot(x, [y*y for y in x])
-plt.title("Time complexity of bubble sort is O(n\u00b2)")
-plt.xlabel("Input")
-plt.ylabel("Time")
+plt.title("Bubble sort time complexity is O(n\u00b2)")
+plt.xlabel("Time")
+plt.ylabel("Input")
 plt.show()
